d863dd5e3711e22fdf471b4e73d624b07bf687f5
Ivaldir Batalha <ivalbat1@gmail.com>
2025-04-21 01:24:12 +0100

    bug-fix(dev): adding mongodb to render.yaml and url to env

--- src/repository/database/db.py
+++ src/repository/database/db.py
@@ -1,6 +1,9 @@
 # db.py
 import motor.motor_asyncio
+import os
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
-db = client["mariposa"]
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
+
+db = client["your_database_name"]
 collection = db["raw_jobs"]
